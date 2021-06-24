d8ae0ef008a5efb1543186c48cca999eb0dd2205
Craig Williams <20600064+MathWiz86@users.noreply.github.com>
2021-06-24 15:56:34 -0700

    BREAKING: Changed Lerp to Interpolation.cs

--- Runtime/Tools/Math/Interpolation/INlerp.cs
+++ Runtime/Tools/Math/Interpolation/INlerp.cs
@@ -1,6 +1,6 @@
 /**************************************************************************************************/
 /*!
-\file   ISlerp.cs
+\file   INlerp.cs
 \author Craig Williams
 \par    Last Updated
         2021-05-31
@@ -8,7 +8,7 @@
         Copyright © 2021 Craig Joseph Williams, All Rights Reserved.
 
 \brief
-  A file for an interface used for marking a type as able to spherically interpolate.
+  A file for an interface used for marking a type as able to normalized-linearly interpolate.
 
 \par Bug List
 
@@ -20,32 +20,32 @@ namespace CodeParadox.Tenor.Math
 {
   /************************************************************************************************/
   /// <summary>
-  /// A useful <see langword="interface"/> for spherically interpolating a value.
-  /// Use in conjunction with the <see cref="Tools.Lerp"/> tools.
+  /// A useful <see langword="interface"/> for normalized-linearly interpolating a value.
+  /// Use in conjunction with the <see cref="Tools.Interpolation"/> tools.
   /// </summary>
   /// <typeparam name="T">The type being lerped. This is the inheriting type.</typeparam>
-  public interface ISlerp<T> where T : ISlerp<T>
+  public interface INlerp<T> where T : INlerp<T>
   {
     /// <summary>
-    /// A function to spherically between two <typeparamref name="T"/>s.
+    /// A function to normalized-linearly between two <typeparamref name="T"/>s.
     /// </summary>
     /// <param name="a">The start <typeparamref name="T"/>, at a <paramref name="t"/> of 0.</param>
     /// <param name="b">The end <typeparamref name="T"/>, at a <paramref name="t"/> of 1.</param>
     /// <param name="t">The interpolation, on a scale of 0 to 1 between the two values.</param>
     /// <returns>Returns the interpolated value.</returns>
-    public abstract T Slerp(T a, T b, float t);
+    public abstract T Nlerp(T a, T b, float t);
 
     /// <summary>
-    /// A function to spherically between two <typeparamref name="T"/>s.
+    /// A function to normalized-linearly between two <typeparamref name="T"/>s.
     /// </summary>
     /// <param name="a">The start <typeparamref name="T"/>, at a <paramref name="t"/> of 0.</param>
     /// <param name="b">The end <typeparamref name="T"/>, at a <paramref name="t"/> of 1.</param>
     /// <param name="t">The interpolation, on a scale of 0 to 1 between the two values.</param>
     /// <returns>Returns the interpolated value.</returns>
-    public abstract T Slerp(T a, T b, double t);
+    public abstract T Nlerp(T a, T b, double t);
 
     /// <summary>
-    /// A function to spherically interpolate between two <typeparamref name="T"/>s,
+    /// A function to normalized-linearly interpolate between two <typeparamref name="T"/>s,
     /// without a clamp.
     /// </summary>
     /// <param name="a">The first <typeparamref name="T"/>, at a <paramref name="t"/> of 0.</param>
@@ -53,10 +53,10 @@ namespace CodeParadox.Tenor.Math
     /// <param name="t">The interpolation between the two values.</param>
     /// <returns>Returns the interpolated value. <paramref name="t"/> values outside the range of
     /// 0 to 1 still affect the returned value.</returns>
-    public abstract T SlerpUnclamped(T a, T b, float t);
+    public abstract T NlerpUnclamped(T a, T b, float t);
 
     /// <summary>
-    /// A function to spherically  interpolate between two <typeparamref name="T"/>s,
+    /// A function to normalized-linearly  interpolate between two <typeparamref name="T"/>s,
     /// without a clamp.
     /// </summary>
     /// <param name="a">The first <typeparamref name="T"/>, at a <paramref name="t"/> of 0.</param>
@@ -64,7 +64,7 @@ namespace CodeParadox.Tenor.Math
     /// <param name="t">The interpolation between the two values.</param>
     /// <returns>Returns the interpolated value. <paramref name="t"/> values outside the range of
     /// 0 to 1 still affect the returned value.</returns>
-    public abstract T SlerpUnclamped(T a, T b, double t);
+    public abstract T NlerpUnclamped(T a, T b, double t);
   }
   /************************************************************************************************/
 }
